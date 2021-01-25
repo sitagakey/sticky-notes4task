4de119d1f130802c42fe5d56074429c0727caaa8
hikikomori <mebeusu@gmail.com>
2021-01-25 21:30:45 +0900

    first-commit

---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+import { sass } from 'sass';
+import { fiber } from 'fibers';
+
+export default {
+    target: 'static',
+    head: {
+        title: 'カダイの付箋',
+        htmlAttrs: {
+            lang: 'ja',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content:
+                    'あなたの持つカダイを管理するためのアプリケーションです',
+            },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ['~/assets/scss/elements'],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        '@nuxt/typescript-build',
+        '@nuxtjs/style-resources',
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
+    styleResources: {
+        scss: ['~/assets/scss/variables.scss', '~/assets/scss/mixin.scss'],
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        loaders: {
+            scss: {
+                implementation: sass,
+                sassOptions: {
+                    fiber,
+                },
+            },
+        },
+    },
+};
